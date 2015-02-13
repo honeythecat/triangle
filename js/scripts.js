d7c553df6753e6541e964b7c3e60d2755c59c0cf
Heather <heather@thisisnotanorg.org>
2015-02-13 12:15:14 -0800

    Add second working spec.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,11 @@
 var triangle = function(sideA, sideB, sideC) {
 
-  var typeOfTriangle = []
 
-  if (((sideA > sideB) && (sideB > sideC) && (sideA > sideC)) != true) {
-    typeOfTriangle.push("not a triangle");
-  // } else {
-  //   ((sideA === sideB && sideB === sideC));
-  //   typeOfTriangle.push("equilateral");
-  // }
+
+  if ((sideA === sideB && sideB === sideC)) {
+    return ("equilateral");
+  } else {
+    return ("not a triangle");
   }
-  return typeOfTriangle;
 
 };
